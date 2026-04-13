ebbcc6abdb0c1242e22a8fe0bf03b1c3870f8217
Atharv Goel <atharvgo@andrew.cmu.edu>
2026-04-13 01:41:07 +0000

    refactor to be able to contain multiple ros packages

---
+++ tauv_launch/launch/main.launch.py
@@ -0,0 +1,166 @@
+import os
+from datetime import datetime
+from pathlib import Path
+
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch.actions import (DeclareLaunchArgument, ExecuteProcess, LogInfo,
+                            SetEnvironmentVariable, TimerAction)
+from launch.substitutions import LaunchConfiguration
+from launch_ros.actions import Node
+
+
+def generate_launch_description():
+    xsens_params_path = os.path.join(
+        get_package_share_directory('xsens_mti_ros2_driver'),
+        'param',
+        'xsens_mti_node.yaml'
+    )
+    dronecan_db_path = os.path.join(
+        get_package_share_directory('tauv_dronecan'),
+        'dronecan_dna.db'
+    )
+    common_share_dir = Path(get_package_share_directory("tauv_core"))
+    common_ekf_file = common_share_dir / "tauv_stateEstimation" / "config" / "ekfFUNNY.yaml"
+    
+    timestamp = datetime.now().strftime('%Y.%m.%d_%H.%M.%S')
+    bag_path = Path("/tauv-mono/ros_ws/bags") / f"rosbag_osprey_{timestamp}"
+
+    watchdog_params = {
+        'esc_topic': '/esc_telemetry',
+        'imu_topic': 'os/sensors/imu_xsens',
+        'system_state_topic': 'watchdog/system_state',
+        'heartbeat_frequency_hz': 1.0,
+        'esc_timeout_s': 1.0,
+        'stale_startup_grace_s': 30.0,
+        'warning_temperature_c': 70.0,
+        'error_temperature_c': 90.0,
+        'error_voltage_v': 12.0,
+        'roll_threshold_deg': 35.0,
+        'pitch_threshold_deg': 35.0,
+        'angular_velocity_threshold_radps': 3,
+        'expected_esc_ids': [100, 101, 102, 103, 104, 105, 106, 107],
+    }
+
+    # --- Launch Description ---
+    return LaunchDescription([
+        DeclareLaunchArgument(
+            'tune', 
+            default_value='False', 
+            description='Enable autotuning for the controller'
+        ),
+
+        # Global Logging Settings for Jetson Orin Performance
+        SetEnvironmentVariable('RCUTILS_LOGGING_USE_STDOUT', '1'),
+        SetEnvironmentVariable('RCUTILS_LOGGING_BUFFERED_STREAM', '1'),
+
+        Node(
+            package='tauv_depth',
+            executable='depth',
+            name='depth_sensor',
+            output='screen',
+            parameters=[{'i2c_bus': 7}]
+        ),
+        Node(
+            package='tauv_dronecan',
+            executable='can_driver',
+            name='dronecan',
+            output='screen',
+            parameters=[{
+                'interface': 'can1',
+                'node_id': 12,
+                'bitrate': 1000000,
+                'esc_count': 8,
+                'command_rate_hz': 100.0,
+                'discovery_time_sec': 15.0,
+                'dna_db_path': dronecan_db_path
+            }]
+        ),
+        Node(
+            package='xsens_mti_ros2_driver',
+            executable='xsens_mti_node',
+            name='xsens_mti_node',
+            output='screen',
+            parameters=[xsens_params_path]
+        ),
+        Node(
+            package='dvl_a50',
+            executable='dvl_a50_sensor', 
+            name='dvl_a50',
+            output='screen',
+            parameters=[{'dvl_ip_address': '192.168.8.114'}]
+        ),
+
+        Node(
+            package='foxglove_bridge',
+            executable='foxglove_bridge',
+            name='foxglove_bridge',
+            parameters=[{'port': 8765, 'address': '0.0.0.0'}]
+        ),
+        ExecuteProcess(            
+            cmd=['ros2', 'bag', 'record', '-a', '-s', 'mcap', '-o', str(bag_path)],
+            output='screen',
+        ),
+        
+        Node(
+            package="tauv_watchdogs",
+            executable="watchdog",
+            name="watchdog",
+            output="screen",
+            parameters=[watchdog_params]
+        ),
+
+        Node(package="tauv_repackagers", executable="imu_converter", name="imu_converter", output="screen"),
+        Node(package="tauv_repackagers", executable="depth_converter", name="depth_converter", output="screen"),
+        Node(package="tauv_repackagers", executable="dvl_converter", name="dvl_converter", output="screen"),
+
+        Node(
+            package='tf2_ros',
+            executable='static_transform_publisher',
+            name='base_link_to_imu',
+            arguments=['0', '0', '0', '3.14159', '0', '0', 'os/base_link', 'imu_link_xsens'],
+            parameters=[{'use_sim_time': True}],
+            output='screen'
+        ),
+        Node(
+            package='tf2_ros',
+            executable='static_transform_publisher',
+            name='base_link_to_depth',
+            arguments=['0', '0', '0', '0', '0', '0', 'os/base_link', 'depth_link'],
+            parameters=[{'use_sim_time': True}],
+            output='screen'
+        ),
+        Node(
+            package='tf2_ros',
+            executable='static_transform_publisher',
+            name='base_link_to_dvl',
+            arguments=['0', '0', '0', '-1.5708', '0.0', '3.14159', 'os/base_link', 'dvl_link'],
+            parameters=[{'use_sim_time': True}],
+            output='screen'
+        ),
+
+        Node(
+            package='tauv_controller',
+            executable='controller',
+            name='controller',
+            parameters=[{'tune': LaunchConfiguration('tune')}],
+            output='screen',
+        ),
+
+        Node(package='tauv_controller', executable='thruster_forces', name='thruster_forces', output='screen'),
+        Node(package='tauv_controller', executable='thruster_rpms', name='thruster_rpms', output='screen'),
+
+        TimerAction(
+            period=5.0,
+            actions=[
+                LogInfo(msg="Starting EKF filter node!!!!!!"),
+                Node(
+                    package="robot_localization",
+                    executable="ekf_node",
+                    name="ekf_filter_node",
+                    parameters=[str(common_ekf_file)],
+                    output="screen",
+                ),
+            ],
+        )
+    ])
--- tauv_watchdogs/src/watchdog.cpp
+++ tauv_watchdogs/src/watchdog.cpp
@@ -1,4 +1,4 @@
-#include "tauv_core/watchdog.h"
+#include "tauv_watchdogs/watchdog.h"
 
 #include <tf2/LinearMath/Matrix3x3.h>
 #include <tf2/LinearMath/Quaternion.h>
