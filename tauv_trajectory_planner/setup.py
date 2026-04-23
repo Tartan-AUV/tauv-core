50906f6112ae7d2d92745f0975f0577429e10dc5
Jason Niow <jniow@andrew.cmu.edu>
2026-04-22 23:32:56 -0400

    added traj planner

---
+++ tauv_trajectory_planner/setup.py
@@ -0,0 +1,29 @@
+from setuptools import find_packages, setup
+
+package_name = 'tauv_trajectory_planner'
+
+setup(
+    name=package_name,
+    version='0.1.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages', ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='root',
+    maintainer_email='root@todo.todo',
+    description='Smooth odometry trajectory planner for TAUV mission targets.',
+    license='TODO: License declaration',
+    extras_require={
+        'test': [
+            'pytest',
+        ],
+    },
+    entry_points={
+        'console_scripts': [
+            'trajectory_planner = tauv_trajectory_planner.trajectory_planner_node:main',
+        ],
+    },
+)
